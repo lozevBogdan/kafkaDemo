0332547d9d38a6a837c30c2ecdc8d8ee152b2a7d
Bogdan Lozev <lozev.bogdan@abv.bg>
2024-07-25 18:20:23 +0300

    POJO class for Serialization and Deserialization

--- src/main/java/com/kafka/springboot/controller/MessageController.java
+++ src/main/java/com/kafka/springboot/controller/MessageController.java
@@ -1,6 +1,6 @@
-package com.kafka.springboot.config.controller;
+package com.kafka.springboot.controller;
 
-import com.kafka.springboot.config.kafka.KafkaProducer;
+import com.kafka.springboot.kafka.KafkaProducer;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/main/java/com/kafka/springboot/kafka/KafkaConsumer.java
+++ src/main/java/com/kafka/springboot/kafka/KafkaConsumer.java
@@ -1,4 +1,4 @@
-package com.kafka.springboot.config.kafka;
+package com.kafka.springboot.kafka;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
